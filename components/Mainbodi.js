e937c2f0bf5cc3a484f5ece62f334856d4d8f9a9
*ladeed <muhammed.ladeed123@gmail.com>
2021-09-27 12:47:44 +0530

    dummy grid implememnted

--- components/Mainbodi.js
+++ components/Mainbodi.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import styles from "../styles/Mainbodi.module.css"
+import { Grid } from '@mui/material'
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const arr=[{id:1,name:"ladeed"},
+{id:2,name:"ldeed"},
+{id:3,name:"leed"},
+{id:4,name:"eed"},
+{id:5,name:"ld"},
+
+]
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 const Mainbody = () => {
     return (
        <div className={styles.container}>
-           <h1>dsfsd</h1>
+           <div className={styles.inputcontainer}>
+        <input/>
+
+           </div>
+
+           <div className={styles.gridcontainer}>
+<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+{arr.map(el=>(<Grid item xs={4}>
+          <Item>{el.id}
+          <h1>{el.name}</h1>
+          </Item>
+        </Grid>))}
+</Grid>
+       </div>
        </div>
 
     )
